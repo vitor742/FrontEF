1a0501675e364828c009d6d2722cd28b00a444f9
Igor-Lima07 <igorlego86@gmail.com>
2025-03-27 15:36:12 -0300

    Arruma o login (de novo) e a adiciona um feedback para o usuario

--- estoque_facil/EF/views.py
+++ estoque_facil/EF/views.py
@@ -12,34 +12,6 @@ def initialize_firebase():
         firebase_admin.initialize_app(cred)
     return firestore.client()
 
-# Essa parte é a parte do login
-
-def login_view(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        access_key = request.POST.get('access_key')
-
-# I
-
-        try:
-            # Inicializa o Firestore
-            db = initialize_firebase()
-
-            # Verifica as credenciais no Firestore
-            users_ref = db.collection('clients')
-            query = users_ref.where('username', '==', username).where('access_key', '==', access_key).get()
-
-            if query:
-                # Se o usuário for encontrado e as credenciais estiverem corretas
-                return JsonResponse({"message": "Login bem-sucedido!"}, status=200)
-            else:
-                return JsonResponse({"error": "Credenciais inválidas!"}, status=401)
-
-        except Exception as e:
-            return JsonResponse({"error": f"Erro ao autenticar: {str(e)}"}, status=500)
-
-    return render(request, 'login.html')  # Renderiza o template de login
-
 def login_view(request):
     return render(request, 'login/login.html')
 

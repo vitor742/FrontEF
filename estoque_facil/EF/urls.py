451e1c30b06d483b96ec395f703ab3e5ce71c301
Igor-Lima07 <igorlego86@gmail.com>
2025-05-12 15:25:18 -0300

    Arruma os erros que deu

--- estoque_facil/EF/urls.py
+++ estoque_facil/EF/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'), 
+    path('', views.login_view, name='login'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('produtos/', views.produtos_view, name='produtos'),
     path('funcionarios/', views.funcionarios_view, name='funcionarios'),
-    path('estabelecer_acao/', views.acao_view, name='estabelecer_acao'),
+    path('estabelecer_acao/', views.listAcao_view, name='estabelecer_acao'),
     path('relatorios/', views.relatorios_view, name='relatorios'),
     path('politica/', views.politica_view, name='politica'),
     path('estatisticas/', views.estatisticas_view, name='estatisticas'),
     path('historico/', views.historico_view, name='historico'),
+    path('addAcao/', views.addAcao, name='addAcao'), 
+    path('addProdutos/', views.addProdutos, name='addProdutos')
+
 ]
